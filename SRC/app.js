142b430e6b9250c22f8fc32b4170ac6e2e750665
juanlezama1 <juanpablolezama92@gmail.com>
2024-02-11 13:40:40 -0300

    Empezando

--- SRC/app.js
+++ SRC/app.js
@@ -7,19 +7,16 @@ import productsRouter from './routes/productsRouter.js'
 const my_app = express ()
 const PORT = 8080
 
-// Cada vez que se solcite algo con la ruta 'static', que busque los archivos estáticos en la carpeta 'public
+// Application middlewares
 
 my_app.use('/static', express.static(__dirname + '/public'))
-
-// Cada vez que se envíen datos en formato JSON, que se parseen a datos objeto/arreglo de objetos de JS. 
-
 my_app.use(express.json())
 
-// Defino las acciones que tomará mi servidor ante las diferentes rutas
+// Router Middlewares
 
-my_app.use('/products', productsRouter) // Cada vez que te consulten por products, andá al router en cuestión
+my_app.use('/products', productsRouter)
 
-// Levanto mi servidor y quedo escuchando solicitudes en el puerto en cuestión.
+// Levanto el server
 
 my_app.listen(PORT, () => {
     console.log(`Escuchando solicitudes en el puerto ${PORT}`)
